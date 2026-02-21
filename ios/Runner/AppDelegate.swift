f0aac3cbfea237adc005e2b4c2a8955af4229fe0
Tuhin Ikbal Eyamin <codereyamin@gmail.com>
2026-02-21 12:28:23 +0600

    mac devices run and update code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,24 +1,21 @@
-import Flutter
 import UIKit
+import Flutter
 import app_links
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      GeneratedPluginRegistrant.register(with: self)
-      
-      if let url = AppLinks.shared.getLink(launchOptions: launchOptions) {
-     
-      AppLinks.shared.handleLink(url: url)
-      return true 
-    }
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    GeneratedPluginRegistrant.register(with: self)
+
+    if let url = AppLinks.shared.getLink(launchOptions: launchOptions) {
+      AppLinks.shared.handleLink(url: url)
+    }
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
